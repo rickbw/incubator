78bb84173ed1d50f477e13ba7757740d69769bf8
Rick Warren <rickbw@users.noreply.github.com>
2014-03-28 09:59:12 -0700

    Added Iterator over BufferedReader

---
+++ src/main/java/rickbw/incubator/lines/LineIterator.java
@@ -0,0 +1,82 @@
+/* Copyright 2014 Rick Warren
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package rickbw.incubator.lines;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Iterator;
+
+import com.google.common.collect.AbstractIterator;
+
+
+/**
+ * An {@link Iterator} over the lines in a {@link BufferedReader}. (In Java 8,
+ * this capability will be built into the JDK.) The reader may be closed
+ * explicitly by calling {@link #close()}. It will be closed implicitly when
+ * the input is fully consumed -- that is, once {@link #hasNext()} returns
+ * false. The final null returned by {@link BufferedReader#readLine()} is
+ * <em>not</em> considered an element by this iterator, and will not be
+ * returned.
+ *
+ * This class implements {@link Iterator}, not {@link Iterable}, because it
+ * accepts an already-opened {@link Reader} (or {@link InputStream}). Thus,
+ * the input is inherently consumed as it is read, and cannot be read a
+ * second time by calling {@link Iterable#iterator()} again. An
+ * {@link Iterable} implementation would have to accept a file path, URL, or
+ * other source, and open the stream itself.
+ */
+public final class LineIterator
+extends AbstractIterator<String>
+implements Iterator<String>, Closeable {
+
+    private final BufferedReader reader;
+
+
+    public LineIterator(final Reader reader) {
+        if (reader instanceof BufferedReader) {
+            this.reader = (BufferedReader) reader;
+        } else {
+            this.reader = new BufferedReader(reader);
+        }
+    }
+
+    public LineIterator(final InputStream stream ) {
+        this.reader = new BufferedReader(new InputStreamReader(stream));
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.reader.close();
+    }
+
+    @Override
+    protected String computeNext() {
+        try {
+            final String line = this.reader.readLine();
+            if (line == null) {
+                endOfData();
+                close();
+            }
+            return line;
+        } catch (final IOException iox) {
+            throw new IllegalStateException(iox);
+        }
+    }
+
+}
