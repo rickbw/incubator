bfe2de2cd3f7515d4d0b97c883abb572de15c1a4
Rick Warren <rickbw@users.noreply.github.com>
2015-01-06 09:13:56 -0800

    Added missing copyright and license to files that were missing it.
    Oops. Checked Git history to verify that starting years are correct.

--- src/main/java/rickbw/incubator/random/RandomSequence.java
+++ src/main/java/rickbw/incubator/random/RandomSequence.java
@@ -1,6 +1,21 @@
+/* Copyright 2014 Rick Warren
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package rickbw.incubator.random;
 
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.Random;
 
 import com.google.common.base.Preconditions;
@@ -65,7 +80,7 @@ public final class RandomSequence {
     }
 
     private RandomSequence(final Random random) {
-        this.random = Preconditions.checkNotNull(random);
+        this.random = Objects.requireNonNull(random);
     }
 
 
